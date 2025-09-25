46e4cd9934b86e5b2ae7cf06b367048a0fb714b1
LuukOV <l.oudevrielink@metachef.nl>
2025-09-25 16:13:55 +0200

    -

--- src/firestore/FirestoreDocument/types.ts
+++ src/firestore/FirestoreDocument/types.ts
@@ -1,12 +1,9 @@
 import type { Timestamp } from "@google-cloud/firestore/build/src/timestamp";
 import type { GeoPoint } from "@google-cloud/firestore/build/src/geo-point";
 
-// @ts-ignore
-import type {
-  DocumentReference,
-  Query,
-  QueryOptions,
-} from "@google-cloud/firestore/build/src/reference";
+import type { Query } from "@google-cloud/firestore/build/src/reference/query";
+import type { QueryOptions } from "@google-cloud/firestore/build/src/reference/query-options";
+import type { DocumentReference } from "@google-cloud/firestore/build/src/reference/document-reference";
 
 import * as protos from "@google-cloud/firestore/build/protos/firestore_v1_proto_api";
 
